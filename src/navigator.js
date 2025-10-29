be4ebf655facf357577f4602cd70c12acea3923a
sumit <sumit.bhowmick@cattlefield.com>
2025-10-29 13:35:08 +0530

    flighbooking agents

---
+++ src/navigator.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './Pages/homeScree1';
+
+// import SettingsScreen from './screens/SettingsScreen';
+// import ProfileScreen from './screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#007AFF',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: { paddingBottom: 5, height: 60 },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
+        {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
